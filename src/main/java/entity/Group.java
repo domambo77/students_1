3347c413ce4b44740e05fd103297fa0212b5ab08
domambo77 <domambo77@mail.ru>
2023-03-18 22:36:51 +0300

    Database configuration added, entity classes created, students page implemented

---
+++ src/main/java/entity/Group.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.Objects;
+
+public class Group {
+    private int id;
+    private String name;
+
+    public Group() {
+    }
+
+    public Group(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Group)) return false;
+        Group group = (Group) o;
+        return getId() == group.getId() && Objects.equals(getName(), group.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
